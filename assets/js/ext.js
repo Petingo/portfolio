afd47957c7ba0490ee4906ebfaee966f3c6ceecf
petingo <eyanghongbin0315@gmail.com>
2020-11-27 16:19:23 +0800

    init

---
+++ assets/js/ext.js
@@ -0,0 +1,12 @@
+// https://typeitjs.com/
+new TypeIt("#typeit-block", {
+    speed: 90,
+    delay: 700,
+    loop: true
+})
+    .type("<span style='color: #F17C67'>Computer Science</span>", {delay: 700})
+    .delete(16)
+    .type("<span style='color: #BEC23F'>Photography</span>", {delay: 700})
+    .delete(11)
+    .type("<span style='color: #0089A7'>Architecture</span>", {delay: 700})
+    .go();
